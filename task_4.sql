a57640180d60f88b93079bd544b00fc12366c6b0
Mahlet656 <mahletshumetie7@gmail.com>
2025-06-30 03:02:39 +0900

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,2 +1,11 @@
-USE alx_book_store;
-SHOW CREATE TABLE books;
+SELECT
+    COLUMN_NAME AS `Field`,
+    COLUMN_TYPE AS `Type`,
+    IS_NULLABLE AS `Null`,
+    COLUMN_KEY AS `Key`,
+    COLUMN_DEFAULT AS `Default`,
+    EXTRA AS `Extra`
+FROM
+    INFORMATION_SCHEMA.COLUMNS
+WHERE
+    TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books';
